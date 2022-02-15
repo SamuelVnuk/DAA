ca22d0c9e6b806cf7bbe516ed4c57bf9feedbb57
SamuelVnuk <73060091+SamuelVnuk@users.noreply.github.com>
2022-02-15 09:14:24 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+        <body>
+            <?php
+
+                if($_POST ['meno'] == "nicolas" && $_POST ['heslo'] == "repan")
+                {
+                    echo "Si prihlaseny";
+                }
+                else {
+                    echo "Nie si prihlaseny";
+                }
+
+
+            ?>    
+        </body>     
+</html>    
